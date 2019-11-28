8809dd11864973f902c1efb0fe98c3495e76dca5
zb <hizengbiao@qq.com>
2019-11-28 20:28:10 +0800

    add tutorial.ipynb and make ipynb to track

---
+++ demo/test.py
@@ -0,0 +1,27 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import cv2
+import random
+from google.colab.patches import cv2_imshow
+
+# import some common detectron2 utilities
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+im = cv2.imread("demo/input.jpg")
+#cv2_imshow(im)
+cfg = get_cfg()
+cfg.merge_from_file("configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can either use the https://dl.fbaipublicfiles.... url, or use the following shorthand
+cfg.MODEL.WEIGHTS = "demo/mask_rcnn_R_50_FPN_3x.pkl"
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+outputs["instances"].pred_classes
+outputs["instances"].pred_boxes
