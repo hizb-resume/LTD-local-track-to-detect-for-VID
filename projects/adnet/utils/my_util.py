a0772c0bdc55e9a44d03caaccafeb71e088a30ac
windows-hizengbiao-laptop <hizengbiao@qq.com>
2020-02-20 00:51:58 +0800

    add iou tool

--- projects/adnet/utils/my_util.py
+++ projects/adnet/utils/my_util.py
@@ -22,6 +22,42 @@ def test():
     # img_files = glob.glob(os.path.join(vidDir,'ILSVRC201*_VID_train_000*','*.mp4'))
     # img_files.sort(key=str.lower)
 
+def cal_iou(box1, box2):
+    r"""
+    iou = list(map(cal_iou, result_boxes, result_boxes_gt))
+    :param box1: x1,y1,w,h
+    :param box2: x1,y1,w,h
+    :return: iou
+    """
+    x11 = box1[0]
+    y11 = box1[1]
+    x21 = box1[0] + box1[2] - 1
+    y21 = box1[1] + box1[3] - 1
+    area_1 = (x21 - x11 + 1) * (y21 - y11 + 1)
+
+    x12 = box2[0]
+    y12 = box2[1]
+    x22 = box2[0] + box2[2] - 1
+    y22 = box2[1] + box2[3] - 1
+    area_2 = (x22 - x12 + 1) * (y22 - y12 + 1)
+
+    x_left = max(x11, x12)
+    x_right = min(x21, x22)
+    y_top = max(y11, y12)
+    y_down = min(y21, y22)
+
+    inter_area = max(x_right - x_left + 1, 0) * max(y_down - y_top + 1, 0)
+    iou = inter_area / (area_1 + area_2 - inter_area)
+    return iou
+
+def cal_success(iou):
+    success_all = []
+    overlap_thresholds = np.arange(0, 1.05, 0.05)
+    for overlap_threshold in overlap_thresholds:
+        success = sum(np.array(iou) > overlap_threshold) / len(iou)
+        success_all.append(success)
+    return np.array(success_all)
+
 def get_ILSVRC_videos_infos():
     '''
     get {gts,img_files(path),name,db_name,nframes}for all videos
