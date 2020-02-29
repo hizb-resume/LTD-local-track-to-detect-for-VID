2c82e04b77a90a4604de8553d2de730ac930b625
windows-hizengbiao-laptop <hizengbiao@qq.com>
2020-02-29 09:48:54 +0800

    update the way of iou process

--- projects/adnet/utils/ADNet_evalTools.py
+++ projects/adnet/utils/ADNet_evalTools.py
@@ -194,6 +194,47 @@ def do_precison(path_pred,path_gt):
     print('iou precision(iou>%.2f): %.2f'%(iou_success_all[14][0],iou_success_all[14][1]))
     print('cls precision(iou>%.2f): %.2f'%(cls_success_all[14][0],cls_success_all[14][1]))
 
+
+def do_precison2(path_pred,path_gt):
+    ious=[]
+    ious_cls=[]
+    vids_pred =read_results_info(path_pred)
+    vids_gt =read_results_info(path_gt)
+    i = 0
+    for j in range(len(vids_gt)):
+        idg=vids_gt[j]['vid_id']
+        if idg!=vids_pred[i]['vid_id']:
+            # print("i_pred: %d, vid_id_pred: %d; j_gt: %d, vid_id_gt: %d."%(i,idv,j,vids_gt[j]['vid_id']))
+            if idg<vids_pred[i]['vid_id']:
+                pass
+            else:
+                print("test, this situation1 is not possible.")
+        else:
+            k=0
+            for l in range(len(vids_gt[j]['frame_id'])):
+                idgf=vids_gt[j]['frame_id'][l]
+                if idgf !=vids_pred[i]['frame_id'][k]:
+                    if idgf < vids_pred[i]['frame_id'][k]:
+                        pass
+                    else:
+                        print("test, this situation2 is not possible.")
+                else:
+                    bboxs_gt=vids_gt[j]['bbox'][l]
+                    bboxs_pred= vids_pred[i]['bbox'][k]
+                    for id_bgt,box in enumerate(bboxs_gt):
+                        id_iou,iou=maxiou(box,bboxs_pred)
+                        ious.append(iou)
+                        if vids_pred[i]['obj_name'][k][id_iou]==vids_gt[j]['obj_name'][l][id_bgt]:
+                            ious_cls.append(iou)
+                        else:
+                            ious_cls.append(0)
+                    k += 1
+            i+=1
+    iou_success_all=cal_success(ious)
+    cls_success_all = cal_success(ious_cls)
+    print('iou precision(iou>%.2f): %.2f'%(iou_success_all[14][0],iou_success_all[14][1]))
+    print('cls precision(iou>%.2f): %.2f'%(cls_success_all[14][0],cls_success_all[14][1]))
+
 if __name__ == "__main__":
     # gen_gt_file('../datasets/data/ILSVRC-vid-eval')
-    do_precison('../datasets/data/ILSVRC-vid-eval-pred.txt','../datasets/data/ILSVRC-vid-eval-gt.txt')
+    do_precison2('../datasets/data/ILSVRC-vid-eval-pred.txt','../datasets/data/ILSVRC-vid-eval-gt.txt')
